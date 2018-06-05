3e7e444046ad406660fe16829a126e4162b7cedd
pockywang <baiq33@163.com>
2018-06-05 08:07:31 +0800

    first

---
+++ app/src/main/java/com/example/pp/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pp.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.btn);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"鬼哥大傻子哈哈哈哈哈",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
